1b5b2da70e52c8edf4f0a09e9d91d1194b411486
Jan0Fu <115988224+Jan0Fu@users.noreply.github.com>
2023-03-18 12:56:17 +0100

    feat: secure endpoints with spring security

---
+++ src/main/java/com/example/registration/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.example.registration.repository;
+
+import com.example.registration.entity.User;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Repository
+@Transactional(readOnly = true)
+public interface UserRepository {
+
+    Optional<User> findByEmail(String email);
+}
---
+++ src/main/java/com/example/registration/service/RegistrationService.java
@@ -0,0 +1,7 @@
+package com.example.registration.service;
+
+public class RegistrationService {
+    public String register(RegistrationRequest request) {
+        return "works";
+    }
+}
